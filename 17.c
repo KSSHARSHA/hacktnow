dc727e51621f4ee9d8342b7f9fb122d2cd316911
KSSHARSHA <56348166+KSSHARSHA@users.noreply.github.com>
2019-10-09 18:27:52 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -3,21 +3,21 @@ int main()
 {
 int c[10], d[10], flag = 0, k1, k2, i, j
   
-printf("Enter array1 size : ");
+printf("Enter array1 size : ") ;
   
-scanf("%d",&k1);
-printf("\nEnter array2 size : ");
+scanf("%d",&k1) ;
+printf("\nEnter array2 size : ") ;
 scanf("%d",&k2);
-printf("\nEnter array1 element : ");
+printf("\nEnter array1 element : ") ;
 for(i = 0;i < k1;i++)
-scanf("%d",&c[i]);
-printf("\nEnter array2 element : ");
+scanf("%d",&c[i]) ;
+printf("\nEnter array2 element : ") ;
 for(i = 0;i < k2;i++)
-scanf("%d",&d[i]);
-printf("\nUnion:");
+scanf("%d",&d[i]) ;
+printf("\nUnion:") ;
   
 for(i = 0;i < k1;i++)
-printf("%d, ",c[i]);
+printf("%d, ",c[i]) ;
 for(i = 0;i < k2;i++)
 {
 for(j = 0;j < k1;j++)
