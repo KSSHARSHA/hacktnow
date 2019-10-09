2d49a041c6c4cb34be7e58a3733d01244f4e4f4f
KSSHARSHA <56348166+KSSHARSHA@users.noreply.github.com>
2019-10-09 18:26:21 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-int c[10], d[10], flag = 0, n1, n2, i, j;
-scanf(" %d ",&n1);
-scanf(" %d ",&n2);
-for(i = 0;i < n1;i++)
-scanf("%d",&c[i]);
-for(i = 0;i < n2;i++)
-scanf("%d",&d[i]);
+int c[10], d[10], flag = 0 , n1 , n2 , i , j ;
+scanf(" %d ", &n1) ;
+scanf(" %d " ,&n2) ;
+for(i = 0;i < n1; i++)
+scanf("%d",&c[i]) ;
+for(i = 0;i < n2; i++)
+scanf("%d",&d[i]) ;
   
   
 printf("Intersection: ");
